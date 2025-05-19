3b795ddaff9b4267d30587d6d894126006bedb8d
0xpritesh <127421747+0xpritesh@users.noreply.github.com>
2025-05-19 16:12:59 +0530

    constantupdate

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,6 @@
 // Fill in the app ID obtained from the Agora console
-const appId = "7fe424e16f2741af80200fb75720e8d3";
+const appId = "put your appID";
 // Fill in the temporary token generated from Agora Console
-const token = "007eJxTYPh3Vezq+pB5D/xTOepn6J/LYX+heUpHxTO4cVoI7zSrVm8FBvO0VBMjk1RDszQjcxPDxDQLAyMDg7Qkc1NzI4NUixTjnztUMhoCGRl+r5zHysgAgSA+J0NZZkpqfklqcQkDAwCy7iEM";
+const token = "put your token";
 // Fill in the channel name you used to generate the token
 const channel = "videotest";
